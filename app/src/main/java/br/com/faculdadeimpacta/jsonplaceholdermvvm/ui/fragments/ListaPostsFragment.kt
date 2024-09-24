2e3baf817f80b745f09014a48d38718a0c8a6205
Fernando Tanaka <fernando.tanaka@outlook.com>
2024-09-23 22:51:24 -0300

    utilizando o viewmodel

--- app/src/main/java/br/com/faculdadeimpacta/jsonplaceholdermvvm/ui/fragments/ListaPostsFragment.kt
+++ app/src/main/java/br/com/faculdadeimpacta/jsonplaceholdermvvm/ui/fragments/ListaPostsFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.faculdadeimpacta.jsonplaceholdermvvm.R
 import br.com.faculdadeimpacta.jsonplaceholdermvvm.databinding.FragmentListaPostsBinding
+import br.com.faculdadeimpacta.jsonplaceholdermvvm.ui.adapters.PostAdapter
+import br.com.faculdadeimpacta.jsonplaceholdermvvm.ui.viewmodels.ListaPostsViewModel
 
 
 class ListaPostsFragment : Fragment() {
 
     private var _binding: FragmentListaPostsBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: ListaPostsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +27,27 @@ class ListaPostsFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentListaPostsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        viewModel.listaPosts.observe(viewLifecycleOwner) { lista ->
+            binding.recyclerViewPosts.adapter = PostAdapter(lista, { userId ->
+                val acao =
+                    ListaPostsFragmentDirections.actionListaPostsFragmentToPostAutorFragment(userId)
+                findNavController().navigate(acao)
+            }) { postId ->
+                val acao =
+                    ListaPostsFragmentDirections.actionListaPostsFragmentToPostComentariosFragment(
+                        postId
+                    )
+                findNavController().navigate(acao)
+            }
+            binding.recyclerViewPosts.layoutManager = LinearLayoutManager(activity)
+        }
+
+        viewModel.getListaPosts()
     }
 
 
